579fbf5123e9b35dc47eaa95f1d339868b7693fb
buris13 <64283936+buris13@users.noreply.github.com>
2025-02-18 21:00:06 +0700

    Update hypridle.sh

--- install-scripts/hypridle.sh
+++ install-scripts/hypridle.sh
@@ -8,7 +8,7 @@ idle=(
 )
 
 #specific branch or release
-idle_tag="v0.1.2"
+idle_tag="v0.1.4"
 
 ## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
 # Determine the directory where the script is located
